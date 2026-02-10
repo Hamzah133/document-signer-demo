8a2ba945655c8f8262a52d1c4acbb60e80e17f08
Hamzah <hashaikjhb024@student.wethinkcode.co.za>
2026-02-10 16:17:04 +0200

    add basic pdf viewer

---
+++ src/app/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+
+@Component({
+  selector: 'app-home',
+  imports: [NgxExtendedPdfViewerModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
